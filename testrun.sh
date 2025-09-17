c8567cf2cf901c002b0c5080d7286fd35a3d660d
IamNirmata <work.harified@gmail.com>
2025-09-16 21:23:52 -0700

    Auto commit at

---
+++ testrun.sh
@@ -0,0 +1,9 @@
+HOSTFILE=/home/hari/b200/Distrbuted_training_tools/test_files/hostfile \
+NPERNODE=1 \
+NP_TOTAL=2 \
+LOGDIR=/tmp/allpairs_test \
+MASTER_PORT_BASE=49000 \
+GEN_SCRIPT=/home/hari/b200/Distrbuted_training_tools/generate_permutations.py \
+LOGDIR=/home/hari/b200/Distrbuted_training_tools/allpairs_logs \
+EXTRA_MPI_ARGS="--oversubscribe" \
+bash /home/hari/b200/Distrbuted_training_tools/run_allpair_test.sh
