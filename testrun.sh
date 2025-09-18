cc6f0f3cc180e0705a06ac423f44e4d73f777f20
IamNirmata <work.harified@gmail.com>
2025-09-17 22:50:25 -0700

    Auto commit at 20250917_225025

--- testrun.sh
+++ testrun.sh
@@ -9,11 +9,14 @@
 # bash /home/hari/b200/Distrbuted_training_tools/run_allpair_test.sh
 
 #git clone https://github.com/IamNirmata/Distrbuted_training_tools.git /opt/Distrbuted_training_tools
+
+home_dir=/opt/cv
+mkdir -p $home_dir
+
 HOSTFILE=/opt/hostfile \
 NPERNODE=8 \
 NP_TOTAL=16 \
-LOGDIR=/data/allpairs_logs \
+LOGDIR=$home_dir/logs \
 MASTER_PORT_BASE=45567 \
 GEN_SCRIPT=/opt/Distrbuted_training_tools/generate_permutations.py \
-LOGDIR=/home/hari/b200/Distrbuted_training_tools/allpairs_logs \
 bash /opt/Distrbuted_training_tools/run_allpair_test.sh
