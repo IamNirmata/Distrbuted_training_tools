8ffc68727c65ccd4ae28827ef4357a74b5864633
IamNirmata <work.harified@gmail.com>
2025-09-26 15:36:12 -0700

    Auto commit at 20250926_153612

--- gitpush.sh
+++ gitpush.sh
@@ -1,5 +1,5 @@
 timestamp=$(date +%Y%m%d_%H%M%S)
-cd /home/hari/b200/validation/Distrbuted_training_tools/
+cd /home/hari/b200/validation/distrbuted_training_tools/
 git add .
 git commit -m "Auto commit at $timestamp"
 git push
