5f6331afeda0c291450a2b380e2359935278ff82
IamNirmata <work.harified@gmail.com>
2025-09-27 15:50:34 -0700

    Auto commit at 20250927_155034

--- jobrunner.sh
+++ jobrunner.sh
@@ -1,9 +1,14 @@
+home_directory=/home/hari/b200/validation/distrbuted_training_tools
+cd $home_directory || exit 1
+
+
 #step 1: Get all node names
 kubectl get nodes --no-headers -o custom-columns=NAME:.metadata.name \
   | grep "gpu" > gpu_nodes.txt
 
 #step 2: Create tmp_job.yml
 
+export gcrjobname=$(kubectl create -f /home/hari/b200/validation/deeplearning_unit_test/dltest.yml \
+  | awk '{print $1}' | cut -d'/' -f2)
 
-
-kubectl create -f 
+kubectl create -f /home/hari/b200/validation/deeplearning_unit_test/dltest.yml 
---
+++ setup_pvc.sh
@@ -0,0 +1,2 @@
+mkdir -p /data/cluster_validation/dltest
+
