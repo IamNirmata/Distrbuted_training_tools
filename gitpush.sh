0e694ef6f6f920c116202514f7b5eb66beda8e3f
IamNirmata <work.harified@gmail.com>
2025-10-29 12:38:38 -0700

    Auto commit at Wed Oct 29 12:38:38 PDT 2025

--- gitpush.sh
+++ gitpush.sh
@@ -1,5 +1,12 @@
-timestamp=$(date +%Y%m%d_%H%M%S)
-cd /home/hari/b200/validation/distrbuted_training_tools/
-git add .
-git commit -m "Auto commit at $timestamp"
-git push
+# timestamp=$(date +%Y%m%d_%H%M%S)
+# cd /home/hari/b200/validation/distrbuted_training_tools/
+# git add .
+# git commit -m "Auto commit at $timestamp"
+# git push
+
+
+
+cd ~/nebius/distrbuted_training_tools    # go to the repo root
+git add .                                # stage ALL changes across subdirectories
+git commit -m "Auto commit at $(date)"   # commit (adds current timestamp automatically)
+git push origin main                     # push to GitHub
