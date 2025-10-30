824f217344c4b8ad2f601841ce7f8baef9f5d782
IamNirmata <work.harified@gmail.com>
2025-10-29 20:45:44 -0700

    Auto commit at Wed Oct 29 20:45:44 PDT 2025

--- llm_finetune/setup_and_data/fsdp.py
+++ llm_finetune/setup_and_data/fsdp.py
@@ -142,7 +142,7 @@ def main() -> None:
     bnb_config = BitsAndBytesConfig(
         load_in_4bit=True,
         bnb_4bit_quant_type="nf4",
-        bnb_4bit_compute_dtype=torch.float16,
+        bnb_4bit_compute_dtype=torch.bfloat16,
         bnb_4bit_use_double_quant=True,
         bnb_4bit_quant_storage=torch.bfloat16,  # Required for FSDP
     )
